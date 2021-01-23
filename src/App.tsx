b06e3113cc7ae60419f246d56b0b1b4b27ea4117
Edward Chung <munjo5746@gmail.com>
2021-01-22 22:14:21 -0500

    hover styles for .selected-region

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
+import { useRef } from "react";
 import "./styles.css";
 
 export default function App() {
+  const selectedRegionRef = useRef<HTMLDivElement | null>(null);
   return (
     <div className="App">
       {[...Array(10).keys()].map(() => {
         return <div></div>;
       })}
+      <div ref={selectedRegionRef} className="selected-region"></div>
     </div>
   );
 }
