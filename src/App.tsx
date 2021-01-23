bcdde0daf74b75403c094d4fad2a09067b7d3d58
Edward Chung <munjo5746@gmail.com>
2021-01-23 14:29:24 -0500

    dragging down will display selected region, mouseup will end the dragging

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,57 @@
-import { useRef } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import "./styles.css";
 
+type Position = {
+  top: number;
+  left: number;
+};
+
+const HEIGHT = 5; // em
 export default function App() {
-  const selectedRegionRef = useRef<HTMLDivElement | null>(null);
+  const [startPosition, setStartPosition] = useState<Position>();
+  const [dragging, setDragging] = useState<boolean>(false);
+  const [numDragBlocks, setNumDragBlocks] = useState<number>(1);
+  const [regionHeight, setRegionHeight] = useState<number>(HEIGHT);
+
   return (
     <div className="App">
-      {[...Array(10).keys()].map(() => {
-        return <div></div>;
+      {[...Array(6).keys()].map((num) => {
+        return (
+          <div
+            key={num}
+            onMouseDown={(e) => {
+              const {
+                left,
+                top
+              } = (e.target as HTMLDivElement).getBoundingClientRect();
+              setStartPosition({
+                left,
+                top
+              });
+              setDragging(true);
+            }}
+            onMouseOver={() => {
+              if (dragging) setNumDragBlocks(numDragBlocks + 1);
+            }}
+            onMouseUp={() => {
+              setDragging(false);
+            }}
+          ></div>
+        );
       })}
-      <div ref={selectedRegionRef} className="selected-region"></div>
+      <div
+        style={
+          startPosition
+            ? {
+                display: "block",
+                top: startPosition.top,
+                height: `${regionHeight * numDragBlocks}em`,
+                pointerEvents: "none"
+              }
+            : { display: "none" }
+        }
+        className="selected-region"
+      ></div>
     </div>
   );
 }
